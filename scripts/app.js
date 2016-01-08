236676192467c320c8c8a187f95d98fab2f6a70e
fraski <fraskib@gmail.com>
2016-01-08 00:45:38 +0000

    fix

--- scripts/app.js
+++ scripts/app.js
@@ -3,9 +3,9 @@ var myApp = angular.module('myApp',[]);
 myApp.controller('myController',function($scope){
     $scope.names=['Fraser','Mollie','Jeff','Steve','Abba'];
     $scope.search = function(){
-        console.log("Searching... not.");  
-        $.getJson('http://mc8.org/fraz/apicall.php',data,function(jsonData)){                       
-              console.log(jsonData);   
+        console.log("Searching... not.");
+        $.getJson('http://mc8.org/fraz/apicall.php',data,function(jsonData){
+              console.log(jsonData);
             });
     }
 });
