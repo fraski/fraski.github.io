84012b803840672def68a0b70bcd81b51dda2d43
fraski <b4017624@my.shu.ac.uk>
2016-01-09 18:57:11 +0000

    update

--- scripts/app.js
+++ scripts/app.js
@@ -10,7 +10,8 @@ Player.prototype.addAccountInfo = function(info){
     this.account_info = info;
 };
 myApp.controller('myController',function($scope){
-    
+    $scope.match_id;
+    $scope.match;
     $scope.matchData;
     $scope.players = [];
     $scope.names=['Fraser','Mollie','Jeff','Steve','Abba'];
@@ -24,6 +25,7 @@ myApp.controller('myController',function($scope){
             $.getJSON('http://mc8.org/fraz/apicall.php?type=GetMatchHistory',function(data){
                 $scope.matchData = data;
                 $scope.players = $scope.matchData.result.matches[0].players;
+                $scope.match_id = $scope.matchData.result.matches[0].match_id;
                 console.log($scope.players[0]);
                 for(y = 0; y < 10; y++){
                     $scope.players[y].account_id = '765' + ($scope.players[y].account_id + 61197960265728);
@@ -45,8 +47,7 @@ myApp.controller('myController',function($scope){
                             }
                     });
                 }
-                                
-               $scope.$apply();
+                $scope.$apply();
             });
             $.getJSON(myurl,function(data){
                 console.log(data);
@@ -59,22 +60,29 @@ myApp.controller('myController',function($scope){
                     });
                 }
                 console.log($scope.gameInfo);
+                $scope.$apply();
             });
             
-
-        })
-    
+            $.getJSON("http://mc8.org/fraz/apicall.php?type=GetMatchDetails&match_id=" + $scope.match_id,function(data){
+                console.log("HERE");
+                $scope.match = data.result;
+                console.log($scope.match);
+                $scope.$apply();
+            });
+            
+        });
     }
+    
     $( document ).ready(function() {
         $.getJSON('http://mc8.org/fraz/apicall.php?type=GetHeroes',function(data){
             console.log(data);
             $scope.heroes = data.result.heroes;
             //$scope.heroes.forEach(function(entry){
-               //console.log(entry); 
+               //console.log(entry);
             //});
             console.log('765' + (73376200+61197960265728));
-        console.log('765' + (22215797+61197960265728));
-        
+            console.log('765' + (22215797+61197960265728));
+
         });
     });
 });
